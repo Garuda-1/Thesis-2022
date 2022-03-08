4dc432a2aeb436bb8a561205817e28ad97e2330a
iandolzhansky <yan.dolganskiy@mail.ru>
2022-03-08 11:08:53 +0300

    Change in experiments order

--- database/init.sql
+++ database/init.sql
@@ -13,3 +13,12 @@ CREATE TABLE runs
     experiment_id BIGINT NOT NULL REFERENCES experiments,
     timestamp     TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+CREATE VIEW experiments_bands AS
+SELECT E.id, E.name, E.start_time, E.end_time, R.min_proof_size, R.max_proof_size
+FROM experiments E
+INNER JOIN (
+    SELECT RS.experiment_id, MIN(RS.proof_size) as min_proof_size, MAX(rs.proof_size) as max_proof_size
+    FROM runs RS
+    GROUP BY RS.experiment_id
+) R ON E.id = R.experiment_id;
