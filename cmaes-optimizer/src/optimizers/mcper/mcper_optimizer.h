75c2f50c91ea6407cd971e0211772eec48a73191
iandolzhansky <yan.dolganskiy@mail.ru>
2022-03-21 12:52:58 +0300

    Add mcper bsat experiment

--- cmaes-optimizer/src/optimizers/mcper/mcper_optimizer.h
+++ cmaes-optimizer/src/optimizers/mcper/mcper_optimizer.h
@@ -5,10 +5,10 @@
 #include "commons/optimizer.h"
 
 struct mcper_optimizer : optimizer {
-    const size_t MAX_ITERATIONS = 1000;
+    const size_t MAX_ITERATIONS = 100;
     const double BUMP_FACTOR = 1.25;
     const double RESCALE_LIMIT = 1e100;
-    const size_t TOP_PAIRS_COUNT = 4;
+    const size_t TOP_PAIRS_COUNT = 1;
     std::vector<double> activity;
 
     mcper_optimizer(std::string path_to_solver, std::string path_to_storage, std::string path_to_dimacs,
