23d2bbda3a6590b73526c6b81b16f06f6d7458cd
Ian Dolzhanskii <yan.dolzhanskiy@gmail.com>
2022-04-12 18:48:31 +0300

    magic

--- run-singularity.sh
+++ run-singularity.sh
@@ -6,9 +6,9 @@ srun \
   --mail-type=END,FAIL \
   --mail-user=yan.dolzhanskiy@gmail.com \
   --ntasks=1 \
-  --nodelist=orthrus-2 \
-  --mem=128G \
-  --cpus-per-task=32 \
+  --nodelist=meduza-1 \
+  --mem=16G \
+  --cpus-per-task=1 \
   --time=100:00:00 \
   --output="output.log" \
-  "singularity exec --bind /nfs/home/ydolzhanskiy/Thesis-2022:/Thesis /nfs/home/ydolzhanskiy/Thesis-2022/experiments-env /nfs/home/ydolzhanskiy/Thesis-2022/run.sh $*"
+  "singularity exec --bind /mnt:/mnt experiments-env ./run.sh $*"
