14bf2a59f7bae69dc1d3c3d65a5673296f5b737c
iandolzhansky <yan.dolganskiy@mail.ru>
2022-04-12 18:14:32 +0300

    Fix run scripts

--- run-singularity.sh
+++ run-singularity.sh
@@ -2,5 +2,15 @@
 set -eu
 
 srun \
-   ... \
-   'singularity exec ips.sif --bind ... ./run.sh'
+  --job-name "B11 Single Thread" \
+  --mail-type=END,FAIL \
+  --mail-user=yan.dolzhanskiy@gmail.com \
+  --ntasks=1 \
+  --nodelist=orthrus-2 \
+  --mem=128G \
+  --cpus-per-task=32 \
+  --time=100:00:00 \
+  --output="output.log" \
+  "singularity exec experiments-env --bind /nfs/home/ydolzhanskiy/Thesis-2022:/Thesis /nfs/home/ydolzhanskiy/Thesis-2022/run.sh $*" \
+  && { sleep 1 ; kill -9 "$PID"; } \
+  || { sleep 1 ; kill -9 "$PID"; }
--- run.sh
+++ run.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
-set -eu
+set -eux
 
-MAX_THREADS=3
-NUM_THREADS=0
+#MAX_THREADS=3
+#NUM_THREADS=0
+#
+#function acquire() {
+#	while [[ $MAX_THREADS -le $NUM_THREADS ]]; do
+#		sleep 1
+#	done
+#	NUM_THREADS=$(( $NUM_THREADS + 1 ))
+#}
+#
+#function release() {
+#	echo "Test $1 finished at $(date)"
+#	NUM_THREADS=$(( $NUM_THREADS - 1 ))
+#}
+#
+#for (( i = 0; i < 9; ++i )); do
+#	config="config_${i}"
+#	acquire && echo "test with config: $config" && sleep 1 && release "$config" &
+#done
+#
+#wait
 
-function acquire() {
-	while [[ $MAX_THREADS -le $NUM_THREADS ]]; do
-		sleep 1
-	done
-	NUM_THREADS=$(( $NUM_THREADS + 1 ))
-}
-
-function release() {
-	echo "Test $1 finished at $(date)"
-	NUM_THREADS=$(( $NUM_THREADS - 1 ))
-}
-
-for (( i = 0; i < 9; ++i )); do
-	config="config_${i}"
-	acquire && echo "test with config: $config" && sleep 1 && release "$config" &
-done
-
-wait
+mkdir /Thesis/cmaes-optimizer/build
+cd /Thesis/cmaes-optimizer/build
+cmake ..
+make glucose runner
+./runner "$@"
