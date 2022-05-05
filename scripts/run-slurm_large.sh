88bdc98afdee810b5e926b5f98f1eb92aa520072
iandolzhansky <yan.dolganskiy@mail.ru>
2022-05-05 22:12:59 +0300

    Add more experiments

--- scripts/run-slurm_large.sh
+++ scripts/run-slurm_large.sh
@@ -7,8 +7,8 @@ srun \
   --mail-user=yan.dolzhanskiy@gmail.com \
   --ntasks=1 \
   --nodelist=meduza-1 \
-  --mem=128G \
-  --cpus-per-task=20 \
+  --mem=256G \
+  --cpus-per-task=24 \
   --time=180:00:00 \
-  --output="output-large.log" \
-  ./run-singularity.sh /Thesis/experiments/6/6_large.json
+  --output="output-large-2.log" \
+  ./run-singularity.sh /Thesis/experiments/6/6_large_2.json
