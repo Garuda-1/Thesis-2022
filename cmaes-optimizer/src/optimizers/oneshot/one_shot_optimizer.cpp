d35904d240a6c9ee4712ac34ae31318b819c8976
iandolzhansky <yan.dolganskiy@mail.ru>
2022-05-07 20:14:06 +0300

    Fix oneshot

--- cmaes-optimizer/src/optimizers/oneshot/one_shot_optimizer.cpp
+++ cmaes-optimizer/src/optimizers/oneshot/one_shot_optimizer.cpp
@@ -16,7 +16,7 @@ ssize_t one_shot_optimizer::fit() {
   std::vector<std::unordered_set<size_t>> var_clauses_p(benchmark.var_count + 1);
   std::vector<std::unordered_set<size_t>> var_clauses_n(benchmark.var_count + 1);
   for (size_t i = 0; i < benchmark.cla_count; ++i) {
-    for (const auto &var : benchmark.clauses[i]) {
+    for (const auto& var : benchmark.clauses[i]) {
       if (var > 0) {
         var_clauses_p[var].insert(i);
       } else {
@@ -25,14 +25,25 @@ ssize_t one_shot_optimizer::fit() {
     }
   }
 
-  std::mt19937 gen;
-  std::bernoulli_distribution dist(0.03);
+  std::vector<double> sample_rates = {0.01, 0.025, 0.05, 0.1};
+  ssize_t result = std::numeric_limits<ssize_t>::max();
+
+  for (double rate : sample_rates) {
+    result = std::min(result, fit_p(rate, sample, options, var_clauses_p, var_clauses_n));
+  }
+}
+
+ssize_t one_shot_optimizer::fit_p(
+    double sample_rate, common::sample& sample, const common::optimizer_options& options,
+    const std::vector<std::unordered_set<size_t>>& var_clauses_p,
+    const std::vector<std::unordered_set<size_t>>& var_clauses_n) {
+  std::bernoulli_distribution dist(sample_rate);
 
   size_t base_var_count = benchmark.var_count;
   size_t new_pairs_count = 0;
   for (size_t var_a = 1; var_a <= base_var_count; ++var_a) {
     for (size_t var_b = 1; var_b <= base_var_count; ++var_b) {
-      for (const auto &c : var_clauses_p[var_a]) {
+      for (const auto& c : var_clauses_p[var_a]) {
         if (var_clauses_p[var_b].find(c) != var_clauses_p[var_b].end() && dist(gen)) {
           add_er(static_cast<int64_t>(var_a), static_cast<int64_t>(var_b));
           ++new_pairs_count;
@@ -46,7 +57,7 @@ ssize_t one_shot_optimizer::fit() {
   }
   for (size_t var_a = 1; var_a <= base_var_count; ++var_a) {
     for (size_t var_b = 1; var_b <= base_var_count; ++var_b) {
-      for (const auto &c : var_clauses_n[var_a]) {
+      for (const auto& c : var_clauses_n[var_a]) {
         if (var_clauses_p[var_b].find(c) != var_clauses_p[var_b].end() && dist(gen)) {
           add_er(-static_cast<int64_t>(var_a), static_cast<int64_t>(var_b));
           ++new_pairs_count;
--- cmaes-optimizer/src/optimizers/oneshot/one_shot_optimizer.h
+++ cmaes-optimizer/src/optimizers/oneshot/one_shot_optimizer.h
@@ -7,11 +7,18 @@
 #include <random>
 
 struct one_shot_optimizer : optimizer {
+  std::mt19937 gen;
+
   one_shot_optimizer(
       std::string path_to_solver, std::string path_to_storage, std::string path_to_dimacs, PGconn* pg_conn,
       int64_t experiment_id);
 
   ssize_t fit() override;
+
+  ssize_t fit_p(
+      double sample_rate, common::sample& sample, const common::optimizer_options& options,
+      const std::vector<std::unordered_set<size_t>>& var_clauses_p,
+      const std::vector<std::unordered_set<size_t>>& var_clauses_n);
 
   void add_er(int64_t lit_a, int64_t lit_b);
 };
